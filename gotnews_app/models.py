d6d4c62106171421cf4ead1054c73189fa0d82a3
Haley Sanders <haleyfsanders@gmail.com>
2018-01-20 14:44:36 -0800

    db, initial versions of models migrated, bug fixes

--- gotnews_app/models.py
+++ gotnews_app/models.py
@@ -7,9 +7,9 @@ class Event(models.Model):
 	start_date = models.DateField(null=True, blank=True)
 	end_date = models.DateField(null=True, blank=True)
 
-    class Meta:
-        verbose_name = 'EVENT'
-        verbose_name_plural = 'EVENTS'
+	class Meta:
+		verbose_name = 'EVENT'
+		verbose_name_plural = 'EVENTS'
 
 class Entity(models.Model):
 	name = models.CharField(max_length=1000)
@@ -19,18 +19,6 @@ class Entity(models.Model):
 		verbose_name='ENTITY'
 		verbose_name_plural='ENTITIES'
 
-class Article(models.Model):
-	date = models.DateField(null=True, blank=True)
-	overall_sentiment = models.DecimalField()
-	title = models.CharField(max_length=1000)
-	url = models.CharField(max_length=1000) # see if there is a url field
-	event = models.ForeignKey(Event, related_name='articles_related')
-	news_source = models.ForeignKey(NewsSource, related_name='articles_related')
-	
-	class Meta:
-		verbose_name='ARTICLE'
-		verbose_name_plural='ARTICLES'
-
 class NewsSource(models.Model):
 	name = models.CharField(max_length=1000)
 
@@ -38,42 +26,57 @@ class NewsSource(models.Model):
 		verbose_name='NEWSSOURCE'
 		verbose_name_plural='NEWSSOURCES'
 
-class NewsSourceEntityAssoc(models.Model):
-	news_source = models.ForeignKey(NewsSource, related_name='entities_related')
-	entity = models.ForeignKey(NewsSource, related_name='news_sources_related')
-	sentiment = models.DecimalField()
+class Article(models.Model):
+	date = models.DateField(null=True, blank=True)
+	overall_sentiment = models.DecimalField(decimal_places=3, max_digits=5)
+	title = models.CharField(max_length=1000)
+	url = models.URLField(max_length=1000) # see if there is a url field
+	event = models.ForeignKey(Event, related_name='articles_related', on_delete=models.CASCADE)
+	news_source = models.ForeignKey(NewsSource, related_name='articles_related', on_delete=models.CASCADE)
 
-	@receiver(post_save)
-	def increment_entity_occurence_count(sender, instance, created, raw, using, update_fields):
-		if created:
-			instance.entity.occurrence_count += 1
-			instance.entity.save()
-	
-	@receiver(pre_delete)
+	class Meta:
+		verbose_name='ARTICLE'
+		verbose_name_plural='ARTICLES'
+
+class NewsSourceEntityAssoc(models.Model):
+	news_source = models.ForeignKey(NewsSource, related_name='entities_related', on_delete=models.CASCADE)
+	entity = models.ForeignKey(Entity, related_name='news_sources_related', on_delete=models.CASCADE)
+	sentiment = models.DecimalField(decimal_places=3, max_digits=5)
+
+	@receiver(post_save, dispatch_uid='<news_source_entity_id>')
+	def increment_entity_occurence_count(sender, instance, created, raw, using, update_fields, **kwargs):
+		if type(instance) == NewsSourceEntityAssoc:
+			if created:
+				instance.entity.occurrence_count += 1
+				instance.entity.save()
+
+	@receiver(pre_delete, dispatch_uid='<news_source_entity_id>')
 	def decrement_entity_occurrence_count(sender, instance, using, **kwargs):
-		instance.entity.occurrence_count = instance.entity.occurrence_count - 1
-		instance.entity.save()
+		if type(instance) == NewsSourceEntityAssoc:
+			instance.entity.occurrence_count = instance.entity.occurrence_count - 1
+			instance.entity.save()
 
 	class Meta:
 		verbose_name='NEWSSOURCEENTITYASSOC'
 		verbose_name_plural='NEWSSOURCEENTITYASSOCS'
 
 class ArticleEntityAssoc(models.Model):
-	article = models.ForeignKey(Article, related_name='entities_related')
-	entity = models.ForeignKey(Entity, related_name='articles_related')
-	sentiment = models.DecimalField()
+	article = models.ForeignKey(Article, related_name='entities_related', on_delete=models.CASCADE)
+	entity = models.ForeignKey(Entity, related_name='articles_related', on_delete=models.CASCADE)
+	sentiment = models.DecimalField(decimal_places=3, max_digits=5)
 
-	@receiver(post_save)
-	def increment_entity_occurence_count(sender, instance, created, raw,using, update_fields):
-		if created:
-			instance.entity.occurrence_count += 1
-			instance.entity.save()
+	@receiver(post_save, dispatch_uid='<article_entity_id>')
+	def increment_entity_occurence_count(sender, instance, created, raw,using, update_fields, **kwargs):
+		if type(instance) == ArticleEntityAssoc:
+			if created:
+				instance.entity.occurrence_count += 1
+				instance.entity.save()
 
-	@receiver(pre_delete)
+	@receiver(pre_delete, dispatch_uid='<article_entity_id>')
 	def decrement_entity_occurrence_count(sender, instance, using, **kwargs):
-		instance.entity.occurrence_count = instance.entity.occurrence_count - 1
-		instance.entity.save()
-
+		if type(instance) == ArticleEntityAssoc:
+			instance.entity.occurrence_count = instance.entity.occurrence_count - 1
+			instance.entity.save()
 	class Meta:
 		verbose_name='ARTICLEENTITYASSOC'
 		verbose_name_plural = 'ARTICLEENTITYASSOCS'
