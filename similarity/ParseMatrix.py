f5c57d8bdbb31ff81b762f433bde41f1ac16b9a3
Sally Ly <sadly@ucdavis.edu>
2018-01-20 21:28:35 -0800

    reads json files

--- similarity/ParseMatrix.py
+++ similarity/ParseMatrix.py
@@ -1,8 +1,14 @@
 from TextSimilar import cosine_sim
 from numpy import argmax
-import csv
+import json
+
+with open('../data_processing/scrape_store.json') as json_file:
+  textArr = json.load(json_file)
 
 testList = []
+for article in textArr:
+  testList.append(article['textData']);
+  # print(article['title']);
 
 similarityMatrix = cosine_sim(*testList).tolist()
 topicsList = []
@@ -12,13 +18,17 @@ delCount = 0
 for simList in similarityMatrix:
   topic = []
   selfIndex = simList.index(max(simList))
-  topic.append(selfIndex)
+  # DEBUG LINE
+  # topic.append(textArr[selfIndex]['title'])
+  topic.append(textArr[selfIndex]);
   for simIndex in range(selfIndex + 1, len(simList)):
-    if (simList[simIndex] < 0.95 and simList[simIndex] > 0.15 and simIndex not in masterList): #refine later
-      topic.append(simIndex)
+    if (simList[simIndex] < 0.95 and simList[simIndex] > 0.4 and simIndex not in masterList): #refine later
+      #DEBUG LINE
+      # topic.append(textArr[simIndex]['title']) 
+      topic.append(textArr[selfIndex])
       delCount = sum(num < simIndex for num in masterList)
       del similarityMatrix[simIndex - delCount]
       masterList.append(simIndex)
   topicsList.append(topic)
 
-print(topicsList)
+return topicsList
