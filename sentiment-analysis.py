b7f95e51ea7d69f934b55d1e4063e73f3ad539f7
Haley Sanders <haleyfsanders@gmail.com>
2018-01-20 22:31:28 -0800

    connecting similarity and sentiment analysis

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,63 +1,55 @@
 # usr/bin/python3
 from google.cloud import language
+import json
+from similarity.ParseMatrix import get_topics_list 
 # from gotnews_app.models import (Event, Entity, Article, NewsSource, NewsSourceEntityAssoc, ArticleEntityAssoc)
 
 def main():
 	# intialize google-cloud language client
 	client = language.LanguageServiceClient()
 
-	friends = ["Sally", "Alex", "Donald", "Haley", "Alec", 
-				"Adrien", "Cynthia", "Jakob", "Kira", 
-				"Bradley", "Srujan", "Radhika", "Shonit", "Anuka"
-	]
-
-	actions = ["smiling", "laughing", "coding", "writing", 
-		"playing Mass Effect", "reading Spiderman comics", 
-		"watching Donnie Darko and then Citizen Kane"
-	]
 	# read in the match data
-	practice_matrix_data = [i for i in range(0,100)]
-	practice_article_data = [
-		friends[i%len(friends)] + " " + actions[i%len(actions)]
-		for i in range(0,100)
-	]
+	with open('./data_processing/scrape_store.json') as file:
+		textArr = json.load(file)
+		topicsList = get_topics_list(textArr)
 
 	# create documents out of each article
-	for i in practice_matrix_data:
-		print(practice_article_data[i])
-		# create Django Event object
-		# event = Event.objects.create()
+	for lst in topicsList:
+		for article in lst:
 
-		# Create Django Article object
-		# article = Article.objects.create()
+			print("!!!!!!!!!!!!!!!!!!!!!!!!!!!! NEW ARTICLE !!!!!!!!!!!!!!!!!!!!!!!!!!")
+			document = language.types.Document(
+				content=article['textData'],
+				language='en',
+				type='PLAIN_TEXT'
+			)
+			# create Django Event object
+			# event = Event.objects.create()
 
-		document = language.types.Document(
-			content=practice_article_data[i],
-			language='en',
-			type='PLAIN_TEXT'
-		)
+			# Create Django Article object
+			# article = Article.objects.create()
 
-		# Get entities
-		response = client.analyze_entities(document=document,  encoding_type='UTF32')
+			# Get entities
+			response = client.analyze_entities(document=document,  encoding_type='UTF32')
+			
+			# for entity in response.entities: 
+			# 	# create Django Entity object
+			# 	print("<--------------" + entity.name + "------------------>")
 
-		# for entity in response.entities: 
-		# 	# create Django Entity object
-		# 	print("<--------------" + entity.name + "------------------>")
+			# Get sentimental
+			# Assign Article's overall sentiment
+			response = client.analyze_sentiment(document=document,  encoding_type='UTF32')
+			print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + str(response.document_sentiment) + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+			# article.sentiment = response.document_sentiment.magnitude
+			# article.save()
 
-		# Get sentimental
-		# Assign Article's overall sentiment
-		response = client.analyze_sentiment(document=document,  encoding_type='UTF32')
-		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + str(response.document_sentiment) + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-		# article.sentiment = response.document_sentiment.magnitude
-		# article.save()
-
-		# get sentimental entities
-		response = client.analyze_entity_sentiment(document=document,  encoding_type='UTF32')
-		for entity in response.entities:
-			# Get-Or-Create Django Entity, ArticleEntityAssoc & set sentiment
-			# entity = Entity.objects.get_or_create()
-			# Do some averaging for the NewsSourceEntityAssoc sentiment value
-			print("###################### " + entity.name + " sentiment:" + str(entity.sentiment.magnitude) + " ###################")
+			# get sentimental entities
+			response = client.analyze_entity_sentiment(document=document,  encoding_type='UTF32')
+			for entity in response.entities:
+				# Get-Or-Create Django Entity, ArticleEntityAssoc & set sentiment
+				# entity = Entity.objects.get_or_create()
+				# Do some averaging for the NewsSourceEntityAssoc sentiment value
+				print("###################### " + entity.name + " sentiment:" + str(entity.sentiment.magnitude) + " ###################")
 
 
 
--- similarity/ParseMatrix.py
+++ similarity/ParseMatrix.py
@@ -1,34 +1,32 @@
-from TextSimilar import cosine_sim
+from .TextSimilar import cosine_sim
 from numpy import argmax
 import json
 
-with open('../data_processing/scrape_store.json') as file:
-  textArr = json.load(file)
+def get_topics_list(textArr):
+  testList = []
+  for article in textArr:
+    testList.append(article['textData']);
+    # print(article['title']);
 
-testList = []
-for article in textArr:
-  testList.append(article['textData']);
-  # print(article['title']);
+  similarityMatrix = cosine_sim(*testList).tolist()
+  topicsList = []
+  masterList = []
+  delCount = 0
 
-similarityMatrix = cosine_sim(*testList).tolist()
-topicsList = []
-masterList = []
-delCount = 0
+  for simList in similarityMatrix:
+    topic = []
+    selfIndex = simList.index(max(simList))
+    # DEBUG LINE
+    # topic.append(textArr[selfIndex]['title'])
+    topic.append(textArr[selfIndex]);
+    for simIndex in range(selfIndex + 1, len(simList)):
+      if (simList[simIndex] < 0.95 and simList[simIndex] > 0.4 and simIndex not in masterList): #refine later
+        #DEBUG LINE
+        # topic.append(textArr[simIndex]['title']) 
+        topic.append(textArr[selfIndex])
+        delCount = sum(num < simIndex for num in masterList)
+        del similarityMatrix[simIndex - delCount]
+        masterList.append(simIndex)
+    topicsList.append(topic)
 
-for simList in similarityMatrix:
-  topic = []
-  selfIndex = simList.index(max(simList))
-  # DEBUG LINE
-  # topic.append(textArr[selfIndex]['title'])
-  topic.append(textArr[selfIndex]);
-  for simIndex in range(selfIndex + 1, len(simList)):
-    if (simList[simIndex] < 0.95 and simList[simIndex] > 0.4 and simIndex not in masterList): #refine later
-      #DEBUG LINE
-      # topic.append(textArr[simIndex]['title']) 
-      topic.append(textArr[selfIndex])
-      delCount = sum(num < simIndex for num in masterList)
-      del similarityMatrix[simIndex - delCount]
-      masterList.append(simIndex)
-  topicsList.append(topic)
-
-return topicsList
+  return topicsList
