2fa3bf1ae0682c623650bff85ae1121f7853fb10
Donald Pinckney <donald_pinckney@icloud.com>
2018-01-21 05:33:04 -0800

    Fix spaces

--- gotnews_app/views.py
+++ gotnews_app/views.py
@@ -5,43 +5,43 @@ from .models import (Event, Entity, NewsSource, Article, NewsSourceEntityAssoc, 
 
 
 def index(request):
-	"""
-	This is the index view.
-	"""
+    """
+    This is the index view.
+    """
 
-	events_dict = dict()
-	events = Event.objects.all().order_by('-start_date')
-	# .annotate(max_sentiment=Max('articles_related__overall_sentiment'), min_sentiment=Min('articles_related__overall_sentiment'))
+    events_dict = dict()
+    events = Event.objects.all().order_by('-start_date')
+    # .annotate(max_sentiment=Max('articles_related__overall_sentiment'), min_sentiment=Min('articles_related__overall_sentiment'))
 
-	# grab the articles for each event that had the max 
-	for event in events:
-		articles = event.articles_related.all().order_by('overall_sentiment')
-		most_positive = articles[0]
-		most_negative = articles[articles.count() - 1]
-		# most_positive = event.articles_related.filter(sentiment=event.max_sentiment).order_by('-date')
-		# most_negative = event.articles_related.filter(sentiment=event.min_sentiment).order_by('-date')
-		# events_dict[event.id] = [most_positive, most_negative]
+    # grab the articles for each event that had the max 
+    for event in events:
+        articles = event.articles_related.all().order_by('overall_sentiment')
+        most_positive = articles[0]
+        most_negative = articles[articles.count() - 1]
+        # most_positive = event.articles_related.filter(sentiment=event.max_sentiment).order_by('-date')
+        # most_negative = event.articles_related.filter(sentiment=event.min_sentiment).order_by('-date')
+        # events_dict[event.id] = [most_positive, most_negative]
 
-  numbers_list = range(1, 50)
-  paginator = Paginator(numbers_list, 7)
-  
-  page = request.GET.get('page')
-  numbers = paginator.get_page(page) #replace numbers_list and numbers with actual articles
-	
-	#Collect Articles
-	# articles = Article.objects.all().order_by('-overall_sentiment')
+    numbers_list = range(1, 50)
+    paginator = Paginator(numbers_list, 7)
 
-	#Collect ArticleEntityAssocs
-	# article_entity_assocs = ArticleEntityAssoc.objects.all().order_by('-sentiment')
+    page = request.GET.get('page')
+    numbers = paginator.get_page(page) #replace numbers_list and numbers with actual articles
+    
+    #Collect Articles
+    # articles = Article.objects.all().order_by('-overall_sentiment')
 
-	#Pair the articles with the most positive and negative sentiments
+    #Collect ArticleEntityAssocs
+    # article_entity_assocs = ArticleEntityAssoc.objects.all().order_by('-sentiment')
 
-	context = {}
-	return render(request,"gotnews_app/index.html", {'numbers':numbers,  'context': context})
+    #Pair the articles with the most positive and negative sentiments
+
+    context = {}
+    return render(request,"gotnews_app/index.html", {'numbers':numbers,  'context': context})
 
 def expand_row(request):
-	"""
-	This will grab and return all data for row expansion.
-	"""
-	# will need the id of the article in question
-	return HttpResponse("This will return the data to expand a row with.")
+    """
+    This will grab and return all data for row expansion.
+    """
+    # will need the id of the article in question
+    return HttpResponse("This will return the data to expand a row with.")
