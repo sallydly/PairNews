132b9edb5d0d6c45b102897ef76bacbac732e0bd
Haley Sanders <haleyfsanders@gmail.com>
2018-01-21 01:03:03 -0800

    some progress on index view

--- gotnews_app/views.py
+++ gotnews_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Max, Min
 from django.http import HttpResponse
 from .models import (Event, Entity, NewsSource, Article, NewsSourceEntityAssoc, ArticleEntityAssoc)
 
@@ -8,7 +9,23 @@ def index(request):
 	This is the index view.
 	"""
 
+	events_dict = dict()
 	events = Event.objects.all().order_by('-start_date')
+	# .annotate(max_sentiment=Max('articles_related__overall_sentiment'), min_sentiment=Min('articles_related__overall_sentiment'))
+
+	# grab the articles for each event that had the max 
+	for event in events:
+		articles = event.articles_related.all().order_by('overall_sentiment')
+		most_positive = articles[0]
+		most_negative = articles[articles.count() - 1]
+		# most_positive = event.articles_related.filter(sentiment=event.max_sentiment).order_by('-date')
+		# most_negative = event.articles_related.filter(sentiment=event.min_sentiment).order_by('-date')
+		# events_dict[event.id] = [most_positive, most_negative]
+
+
+
+
+
 	
 	#Collect Articles
 	# articles = Article.objects.all().order_by('-overall_sentiment')
